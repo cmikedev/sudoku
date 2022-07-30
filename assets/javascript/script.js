375df275014c4fbc9468afc845ff6b91369a0dc4
cmikedev <carberrm@gmail.com>
2022-07-30 07:14:06 +0000

    Add numbers to selection area and created board divs

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,7 +3,7 @@ let tileSelected = null;
 
 let wrongGuesses = 0;
 
-let puzzle = {
+let puzzle = [
     "43526978-",
     "68257149-",
     "197834562",
@@ -13,9 +13,9 @@ let puzzle = {
     "519326874",
     "248957136",
     "763418259"
-}
+]
 
-let puzzleSolution = {
+let puzzleSolution = [
     "435269781",
     "682571493",
     "197834562",
@@ -25,4 +25,31 @@ let puzzleSolution = {
     "519326874",
     "248957136",
     "763418259"
+]
+
+
+window.onload = function() {
+    createGame();
+}
+
+function createGame() {
+    // populating the board by creating 81 divs
+    for (let row = 0; row < 9; row++) {
+        for (let column = 0; column < 9; column++) {
+            let boardTile = document.createElement("div");
+            boardTile.id = row.toString() + "-" + column.toString();
+            boardTile.classList.add("board-tile");
+            document.getElementById("board").appendChild(boardTile);
+        }
+    }
+
+    // populating the "number-selection" div in the html by creating 9 divs
+    for (let i = 1; i <= 9; i++) {
+        let number = document.createElement("div");
+        number.id = i;
+        number.innerText = i;
+        number.classList.add("number-selection-tile");
+        document.getElementById("number-selection").appendChild(number);
+    }
+
 }
